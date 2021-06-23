26ce9406e3ae8becd423dae9e48b586b78dcf5ad
shin <chulgs15@naver.com>
2021-06-23 09:28:45 +0900

    linked list 개발 중

---
+++ algorithm/src/main/java/org/example/algorithm/LinkedList.java
@@ -0,0 +1,13 @@
+package org.example.algorithm;
+
+public class LinkedList<E> {
+    class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
---
+++ algorithm/src/test/java/org/example/algorithm/LinkedLIstTest.java
@@ -0,0 +1,13 @@
+package org.example.algorithm;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LinkedLIstTest {
+    // 메서드 기준으로 테스트를 작성해 보자.
+    @Test
+    @DisplayName("노드가 ")
+    public void test1() {
+
+    }
+}
