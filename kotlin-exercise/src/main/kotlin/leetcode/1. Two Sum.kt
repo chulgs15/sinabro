d0c6583ba9974e62c811ca64512a86d301fa4404
chulgs15 <chulgs15@naver.com>
2021-08-15 05:11:11 +0900

    kotlin 공부

--- kotlin-exercise/src/main/kotlin/leetcode/1. Two Sum.kt
+++ kotlin-exercise/src/main/kotlin/leetcode/1. Two Sum.kt
@@ -2,6 +2,27 @@ package leetcode
 
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
+        val length: Int = nums.size
+
+        if (length == 1) {
+            return intArrayOf(0)
+        }
+
+        if (length == 2) {
+            return intArrayOf(0,1)
+        }
+
+        var result = 0
+        for (i in 0..length - 2) {
+            var tmpValue = nums[i]
+            for (j in i..length - 1) {
+                tmpValue += nums[j]
+                if (tmpValue > target) {
+                    break;
+                }
+            }
+        }
+
         return intArrayOf(1,2,3)
     }
 }
--- kotlin-exercise/src/test/kotlin/TwoSumTest.kt
+++ kotlin-exercise/src/test/kotlin/TwoSumTest.kt
@@ -1,10 +1,38 @@
 import leetcode.IntArrayLeetCode
-import org.testng.annotations.Test
+import leetcode.Solution
+import kotlin.test.Test
 
 class TwoSumTest {
     @Test
     fun 배열길이가_1이면_결과는_동일하다() {
+        val nums: IntArray = intArrayOf(3)
+        val target = 3
 
+        val solution = Solution()
+        val twoSum = solution.twoSum(nums, target)
+
+        assert(twoSum == intArrayOf(0))
     }
 
+    @Test
+    fun 배열길이와_2라면_결과는_동일하다() {
+        val nums: IntArray = intArrayOf(3, 3)
+        val target = 6
+
+        val solution = Solution()
+        val twoSum = solution.twoSum(nums, target)
+
+        assert(twoSum == intArrayOf(0))
+    }
+
+    @Test
+    fun case_1() {
+        val nums: IntArray = intArrayOf(1, 2, 2, 4)
+        val target = 3
+
+        val solution = Solution()
+        val twoSum = solution.twoSum(nums, target)
+
+        assert(twoSum == intArrayOf(2,3))
+    }
 }
